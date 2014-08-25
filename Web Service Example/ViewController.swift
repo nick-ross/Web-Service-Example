386d923546d3eeaa7c618d13ee72bdc34b1abca2
nick-ross <mx5433@gmail.com>
2014-08-25 15:39:20 -0500

    added activity indicator

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    
+    @IBOutlet var myNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Implement indicator
+        let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
+        activityIndicator.center = self.view.center
+        self.view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "http:/graph.facebook.com/bobdylan",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+                
+                if let myName = responseObject.valueForKey("name") as? String {
+                    self.myNameLabel.text = myName
+                }
+                activityIndicator.stopAnimating()
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Nick Ross on 8/22/14.
+//  Copyright (c) 2014 Nick Ross. All rights reserved.
+//
+
+#ifndef Web_Service_Example_bridge_h
+#define Web_Service_Example_bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+#endif
